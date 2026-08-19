c7f426fe2ef7776161d0b70a09ae3f9b19d91b8f
aliibrahim123 <ali.ibrahim.dev.1548@gmail.com>
2026-08-19 22:06:04 +0300

    refactored a tone

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,217 +1,251 @@
 use proc_macro2::{
-	Delimiter, Group, Ident, Punct, Spacing, Span, TokenStream, TokenTree, token_stream::IntoIter,
+	Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree,
+	token_stream::IntoIter,
 };
 use quote::{TokenStreamExt, quote, quote_spanned};
 
 #[proc_macro]
 pub fn chunk(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = TokenStream::from(input).into_iter();
-	let mut result = TokenStream::new();
-	chunk_impl(input, &mut result, false);
-	result.into()
+	chunk_impl(TokenStream::from(input), false).unwrap_or(TokenStream::new()).into()
 }
 
 #[proc_macro]
 pub fn chunk_spanned(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = TokenStream::from(input).into_iter();
-	let mut result = TokenStream::new();
-	chunk_impl(input, &mut result, true);
-	result.into()
+	chunk_impl(TokenStream::from(input), true).unwrap_or(TokenStream::new()).into()
 }
 
-fn chunk_impl(mut input: IntoIter, result: &mut TokenStream, spanned: bool) {
-	let stream_ident = match input.next() {
-		Some(TokenTree::Ident(ident)) => ident,
-		Some(token) => return error(result, token.span(), "expected an identifier"),
-		_ => return error(result, Span::call_site(), "expected an identifier"),
-	};
-	match input.next() {
-		Some(TokenTree::Punct(punct)) if punct.as_char() == ',' => {}
-		Some(token) => return error(result, token.span(), "expected `,`"),
-		_ => return error(result, Span::call_site(), "expected `,`"),
-	};
-	let span = if spanned {
-		let mut span = TokenStream::new();
-		loop {
-			let Some(token) = input.next() else {
-				return error(result, Span::call_site(), "expected `,`");
-			};
-			if matches!(&token, TokenTree::Punct(punct) if punct.as_char() == ',') {
+struct Cursor {
+	iter: IntoIter,
+	cur_token: Option<TokenTree>,
+	res: TokenStream,
+	end_span: Span,
+}
+
+impl Cursor {
+	fn new(iter: TokenStream, end_span: Span) -> Self {
+		Self { iter: iter.into_iter(), res: TokenStream::new(), cur_token: None, end_span }
+	}
+	fn next(&mut self) -> Option<TokenTree> {
+		self.cur_token.take().or_else(|| self.iter.next())
+	}
+	fn peek(&mut self) -> Option<&TokenTree> {
+		if self.cur_token.is_none() {
+			self.cur_token = self.iter.next();
+		}
+		self.cur_token.as_ref()
+	}
+	fn peek_kw(&mut self, kw: &str) -> bool {
+		match self.peek() {
+			Some(TokenTree::Ident(ident)) => ident == kw,
+			_ => false,
+		}
+	}
+	fn add<T>(&mut self, tokens: impl IntoIterator<Item = T>)
+	where
+		TokenStream: Extend<T>,
+	{
+		self.res.extend(tokens);
+	}
+	fn expected(&mut self, expected: &str, span: Option<Span>) {
+		let span = span.unwrap_or(self.end_span);
+		let msg = &format!("expected {expected}");
+		self.res.extend(quote_spanned! { span => ::core::compile_error!(#msg); })
+	}
+	fn eat_ident(&mut self) -> Option<Ident> {
+		match self.next() {
+			Some(TokenTree::Ident(ident)) => return Some(ident),
+			t => self.expected("identifier", t.map(|t| t.span())),
+		};
+		None
+	}
+	fn eat_punct(&mut self, char: char) -> bool {
+		match self.next() {
+			Some(TokenTree::Punct(punct)) if punct.as_char() == char => return true,
+			t => self.expected(&format!("`{char}`"), t.map(|t| t.span())),
+		};
+		false
+	}
+	fn eat_brace(&mut self) -> Option<Group> {
+		match self.next() {
+			Some(TokenTree::Group(g)) if g.delimiter() == Delimiter::Brace => return Some(g),
+			t => self.expected("`(`", t.map(|t| t.span())),
+		}
+		None
+	}
+	fn eat_until(
+		&mut self, expected: &str, pred: impl Fn(&TokenTree) -> bool,
+	) -> Option<TokenStream> {
+		let mut tokens = TokenStream::new();
+		while let Some(token) = self.peek() {
+			if pred(&token) {
 				break;
 			}
-			span.append(token);
+			tokens.append(self.next().unwrap());
 		}
-		if span.is_empty() {
-			return error(result, Span::call_site(), "expected an expression");
+		if tokens.is_empty() {
+			let span = self.peek().map(|t| t.span());
+			self.expected(expected, span);
 		}
-		span
-	} else {
-		quote! { ::chunked_quote::__private::Span::call_site() }
-	};
-	let stream = quote_stream(input, &stream_ident, Span::call_site());
-	result.extend(quote! { #[allow(unused_braces)] { let __span = #span; #stream #stream_ident } });
+		Some(tokens)
+	}
 }
 
-fn emit_punct(punct: &Punct, stream_ident: &Ident, result: &mut TokenStream) {
+fn is_punct(token: &TokenTree, char: char) -> bool {
+	matches!(token, TokenTree::Punct(punct) if punct.as_char() == char)
+}
+
+fn chunk_impl(input: TokenStream, spanned: bool) -> Option<TokenStream> {
+	let mut cur = Cursor::new(input, Span::call_site());
+
+	let stream_ident = cur.eat_ident()?;
+	cur.eat_punct(',').then_some(())?;
+
+	if spanned {
+		let span = cur.eat_until("an expression", |t| is_punct(t, ','))?;
+		cur.eat_punct(',').then_some(())?;
+		cur.add(quote! { let __span = #span; });
+	} else {
+		cur.add(quote! { let __span = ::chunked_quote::__private::Span::call_site(); })
+	};
+
+	quote_stream(&mut cur, &stream_ident);
+	let res = cur.res;
+	Some(quote! { #[allow(unused_braces)] { #res #stream_ident } })
+}
+
+fn quote_punct(cur: &mut Cursor, punct: &Punct, stream_ident: &Ident) {
 	let spacing = match punct.spacing() {
 		Spacing::Joint => quote! { ::chunked_quote::__private::Spacing::Joint },
 		Spacing::Alone => quote! { ::chunked_quote::__private::Spacing::Alone },
 	};
-	let punct = punct.as_char();
-	result.extend(quote! {
-		let mut __punct = ::chunked_quote::__private::Punct::new(#punct, #spacing);
+	let ch = punct.as_char();
+	cur.add(quote! {
+		let mut __punct = ::chunked_quote::__private::Punct::new(#ch, #spacing);
 		__punct.set_span(__span);
 		#stream_ident.extend(Some(__punct));
 	})
 }
 
-fn quote_stream(mut stream: IntoIter, stream_ident: &Ident, end_span: Span) -> TokenStream {
-	let mut result = TokenStream::new();
-	while let Some(token) = stream.next() {
-		match token {
-			TokenTree::Ident(ident) => {
-				let ident = ident.to_string();
-				let ident = match ident.strip_prefix("r#") {
-					Some(ident) => {
-						quote! { ::chunked_quote::__private::Ident::new_raw(#ident, __span) }
-					}
-					None => quote! { ::chunked_quote::__private::Ident::new(#ident, __span) },
-				};
-				result.extend(quote! { #stream_ident.extend(Some(#ident)); })
-			}
-			TokenTree::Literal(lit) => {
-				let lit = lit.to_string();
-				result.extend(quote! {
-					let mut __lit = <::chunked_quote::__private::Literal as ::core::str::FromStr>
-						::from_str(#lit).unwrap();
-					__lit.set_span(__span);
-					#stream_ident.extend(Some(__lit));
-				})
-			}
-			TokenTree::Punct(punct) => {
-				if punct.as_char() == '#' {
-					handle_directive(&mut stream, stream_ident, end_span, &mut result);
-				} else {
-					emit_punct(&punct, stream_ident, &mut result)
-				}
-			}
-			TokenTree::Group(group) => {
-				let delimiter = match group.delimiter() {
-					Delimiter::Parenthesis => {
-						quote! { ::chunked_quote::__private::Delimiter::Parenthesis }
-					}
-					Delimiter::Brace => quote! { ::chunked_quote::__private::Delimiter::Brace },
-					Delimiter::Bracket => quote! { ::chunked_quote::__private::Delimiter::Bracket },
-					Delimiter::None => quote! { ::chunked_quote::__private::Delimiter::None },
-				};
-				let stream =
-					quote_stream(group.stream().into_iter(), stream_ident, group.span_close());
-				result.extend(quote! {
-					let __group = {
-						let mut #stream_ident = ::chunked_quote::__private::TokenStream::new();
-						#stream
-						let mut __group = ::chunked_quote::__private::Group::new(#delimiter, #stream_ident);
-						__group.set_span(__span);
-						__group
-					};
-					#stream_ident.extend(Some(__group));
-				})
-			}
-		}
-	}
-	result
+fn quote_ident(cur: &mut Cursor, ident: &Ident, stream_ident: &Ident) {
+	let name = ident.to_string();
+	match name.strip_prefix("r#") {
+		Some(name) => cur.add(quote! {
+			#stream_ident.extend(Some(::chunked_quote::__private::Ident::new_raw(#name, __span)));
+		}),
+		None => cur.add(quote! {
+			#stream_ident.extend(Some(::chunked_quote::__private::Ident::new(#name, __span)));
+		}),
+	};
 }
 
-fn error(result: &mut TokenStream, span: Span, expected: &str) {
-	result.extend(quote_spanned! {
-		span => ::core::compile_error!(#expected);
+fn quote_literal(cur: &mut Cursor, lit: &Literal, stream_ident: &Ident) {
+	let text = lit.to_string();
+	cur.add(quote! {
+		let mut __lit = <::chunked_quote::__private::Literal as ::core::str::FromStr>
+			::from_str(#text).unwrap();
+		__lit.set_span(__span);
+		#stream_ident.extend(Some(__lit));
 	})
 }
 
-fn eat_expr_and_block(
-	stream: &mut IntoIter, result: &mut TokenStream, end_span: Span,
-) -> Option<(Vec<TokenTree>, Group)> {
-	let mut expr = Vec::new();
-	while let Some(token) = stream.next() {
-		if let TokenTree::Group(group) = &token
-			&& group.delimiter() == Delimiter::Brace
-		{
-			return if expr.is_empty() {
-				error(result, group.span_open(), "expected an expression");
-				None
-			} else {
-				let TokenTree::Group(group) = token else { unreachable!() };
-				Some((expr, group))
-			};
-		}
-		expr.push(token);
-	}
-	error(result, end_span, "expected `{`");
-	None
-}
-
-fn handle_match(
-	stream: &mut IntoIter, stream_ident: &Ident, match_span: Span, end_span: Span,
-	result: &mut TokenStream,
-) {
-	let Some((expr, block)) = eat_expr_and_block(stream, result, end_span) else {
-		return;
+fn quote_group(cur: &mut Cursor, group: &Group, stream_ident: &Ident) {
+	let delimiter = match group.delimiter() {
+		Delimiter::Parenthesis => quote! { ::chunked_quote::__private::Delimiter::Parenthesis },
+		Delimiter::Brace => quote! { ::chunked_quote::__private::Delimiter::Brace },
+		Delimiter::Bracket => quote! { ::chunked_quote::__private::Delimiter::Bracket },
+		Delimiter::None => quote! { ::chunked_quote::__private::Delimiter::None },
 	};
-	let mut stream = block.stream().into_iter();
-	let mut arms = TokenStream::new();
-	let mut pat = Vec::new();
-	while let Some(token) = stream.next() {
-		if let TokenTree::Group(group) = &token
-			&& group.delimiter() == Delimiter::Brace
-		{
-			let stream = quote_stream(group.stream().into_iter(), stream_ident, group.span_close());
-			arms.extend(pat);
-			arms.extend(Some(Group::new(Delimiter::Brace, stream)));
-			pat = Vec::new();
-		} else {
-			pat.push(token);
-		}
-	}
-	arms.extend(pat);
-	let match_ident = Ident::new("match", match_span);
-	result.extend(quote! { #match_ident #(#expr)* { #arms }; });
+	let mut inner_cur = Cursor::new(group.stream(), group.span_close());
+	quote_stream(&mut inner_cur, stream_ident);
+	let inner = inner_cur.res;
+	cur.add(quote! {
+		let mut __group = {
+			let mut #stream_ident = ::chunked_quote::__private::TokenStream::new();
+			#inner
+			::chunked_quote::__private::Group::new(#delimiter, #stream_ident)
+		};
+		__group.set_span(__span);
+		#stream_ident.extend(Some(__group));
+	})
 }
 
-fn handle_directive(
-	stream: &mut IntoIter, stream_ident: &Ident, end_span: Span, result: &mut TokenStream,
-) {
-	match stream.next() {
-		Some(TokenTree::Punct(punct)) if punct.as_char() == '#' => {
-			emit_punct(&punct, stream_ident, result)
+fn quote_stream(cur: &mut Cursor, stream_ident: &Ident) {
+	while let Some(token) = cur.next() {
+		match token {
+			TokenTree::Ident(ident) => quote_ident(cur, &ident, stream_ident),
+			TokenTree::Literal(lit) => quote_literal(cur, &lit, stream_ident),
+			TokenTree::Punct(p) if p.as_char() == '#' => {
+				handle_directive(cur, stream_ident);
+			}
+			TokenTree::Punct(punct) => quote_punct(cur, &punct, stream_ident),
+			TokenTree::Group(group) => quote_group(cur, &group, stream_ident),
 		}
+	}
+}
+
+fn kw_expr_body(cur: &mut Cursor, ident: Ident, stream_ident: &Ident) -> Option<()> {
+	cur.add(Some(ident));
+	let expr = cur.eat_until("an expression", |t| is_punct(t, '#'))?;
+	cur.add(expr);
+	body(cur, true, stream_ident)
+}
+fn body(cur: &mut Cursor, eat_hash: bool, stream_ident: &Ident) -> Option<()> {
+	if eat_hash {
+		cur.eat_punct('#').then_some(())?
+	}
+	let body = cur.eat_brace()?;
+	let mut inner_cur = Cursor::new(body.stream(), body.span_close());
+	quote_stream(&mut inner_cur, stream_ident);
+	cur.add(Some(Group::new(Delimiter::Brace, inner_cur.res)));
+	Some(())
+}
+
+fn is_brace(token: &TokenTree) -> bool {
+	matches!(token, TokenTree::Group(group) if group.delimiter() == Delimiter::Brace)
+}
+
+fn handle_match(cur: &mut Cursor, ident: Ident, stream_ident: &Ident) -> Option<()> {
+	cur.add(Some(ident));
+	let expr = cur.eat_until("an expression", |t| is_brace(t))?;
+	cur.add(expr);
+	let arms = cur.eat_brace()?;
+	let mut arms_cur = Cursor::new(arms.stream(), arms.span_close());
+	while let Some(token) = arms_cur.next() {
+		if is_punct(&token, '#') {
+			body(&mut arms_cur, false, stream_ident);
+		} else {
+			arms_cur.add(Some(token));
+		}
+	}
+	cur.add(Some(Group::new(Delimiter::Brace, arms_cur.res)));
+	Some(())
+}
+
+fn handle_directive(cur: &mut Cursor, stream_ident: &Ident) -> Option<()> {
+	match cur.next() {
+		Some(TokenTree::Punct(p)) if p.as_char() == '#' => quote_punct(cur, &p, stream_ident),
+		Some(TokenTree::Group(group)) if group.delimiter() == Delimiter::Brace => cur.add(quote! {
+			::chunked_quote::__private::ToTokens::to_tokens(&#group, &mut #stream_ident);
+		}),
 		Some(TokenTree::Ident(ident)) => match &*ident.to_string() {
-			"if" | "else" | "for" | "while" | "loop" => {
-				let Some((expr, block)) = eat_expr_and_block(stream, result, end_span) else {
-					return;
-				};
-				let stream =
-					quote_stream(block.stream().into_iter(), stream_ident, block.span_close());
-				result.extend(quote! { #ident #(#expr)* { #stream } })
+			"if" | "for" | "while" | "loop" => kw_expr_body(cur, ident, stream_ident)?,
+			"else" => {
+				if cur.peek_kw("if") {
+					kw_expr_body(cur, ident, stream_ident)?
+				} else {
+					body(cur, true, stream_ident)?
+				}
 			}
-			"match" => handle_match(stream, stream_ident, ident.span(), end_span, result),
 			"do" => {
-				let group = match stream.next() {
-					Some(TokenTree::Group(group)) if group.delimiter() == Delimiter::Brace => group,
-					Some(token) => return error(result, token.span(), "expected `{`"),
-					_ => return error(result, end_span, "expected `{`"),
-				};
-				result.extend(quote! {{ let #stream_ident = &mut #stream_ident; #group }});
+				let block = cur.eat_brace()?;
+				cur.add(quote! {{ let #stream_ident = &mut #stream_ident; #block }})
 			}
-			_ => result.extend(quote! {
+			"match" => handle_match(cur, ident, stream_ident)?,
+			_ => cur.add(quote! {
 				::chunked_quote::__private::ToTokens::to_tokens(&#ident, &mut #stream_ident);
 			}),
 		},
-		Some(TokenTree::Group(group)) if group.delimiter() == Delimiter::Brace => {
-			result.extend(quote! {
-				::chunked_quote::__private::ToTokens::to_tokens(&#group, &mut #stream_ident);
-			})
-		}
-		Some(token) => error(result, token.span(), "expected identifier, `#` or `{`"),
-		_ => error(result, end_span, "expected identifier, `#` or `{`"),
-	}
+		t => cur.expected("an identifier, `#` or `{`", t.map(|t| t.span())),
+	};
+	Some(())
 }
